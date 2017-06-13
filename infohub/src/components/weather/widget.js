effd647846f26bc01abae726b11b17dea506685d
Anton Levholm <anton.badminton@gmail.com>
2017-06-13 12:16:26 +0200

    add weather widget

---
+++ infohub/src/components/weather/widget.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../../App.css';
+
+class Weather extends Component {
+  render() {
+    return (
+      <div className="widget weather">
+
+      </div>
+    );
+  }
+}
+
+export default Weather;
