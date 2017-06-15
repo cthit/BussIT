d60ab7933275a6878c2760acde465722a2f9fca5
Anton Levholm <anton.badminton@gmail.com>
2017-06-15 22:29:48 +0200

    separate implementation for Västtrafik API into module

--- flask-app/InfoHubAPI/app.py
+++ flask-app/InfoHubAPI/app.py
@@ -1,9 +1,5 @@
 from flask import Flask
-import datetime
-import json
-import requests
-import base64
-from .api_keys import API
+from .vasttrafik import getVasttrafikJSON
 app = Flask(__name__)
 
 @app.route('/')
@@ -13,40 +9,3 @@ def hello_world():
 @app.route('/vasttrafik/<id>')
 def vasttrafik(id=None):
     return getVasttrafikJSON(id)
-
-def getVasttrafikJSON(id):
-    authKey = getAccsessTokenVasttrafik()
-    date = '20' + str(datetime.datetime.now().strftime("%y-%m-%d"))
-    time = str(datetime.datetime.now().strftime("%H:%M"))
-    host = 'https://api.vasttrafik.se'
-    baseurl = '/bin/rest.exe/v2'
-    headers = {
-        'Authorization': 'Bearer ' + authKey
-    }
-    request = {
-        'id': id,
-        'date': date,
-        'time': time,
-        'format': 'json',
-    }
-    requestURL = host + baseurl + '/departureBoard'
-    r = requests.get(requestURL, params=request, headers=headers)
-    return json.dumps(r.json())
-
-def getAccsessTokenVasttrafik():
-    url = 'https://api.vasttrafik.se/token' # Set destination URL here
-    key = API['vasttrafik']['key']
-    secret = API['vasttrafik']['secret']
-    encoded = base64.b64encode((key + ":" + secret).encode()).decode()
-    post_fields = {
-        'grant_type': 'client_credentials',
-        'scope': '1'
-    }
-    headers = {
-        'Authorization': 'Basic ' + encoded,
-        'Content-Type': 'application/x-www-form-urlencoded',
-    }     # Set POST fields here
-    r = requests.post(url, data=post_fields, headers=headers).json()
-    r = json.dumps(r)
-    access_token = json.loads(r)['access_token']
-    return access_token
---
+++ flask-app/InfoHubAPI/vasttrafik.py
@@ -0,0 +1,42 @@
+import datetime
+import json
+import requests
+import base64
+from .api_keys import API
+
+def getVasttrafikJSON(id):
+    authKey = getAccsessTokenVasttrafik()
+    date = '20' + str(datetime.datetime.now().strftime("%y-%m-%d"))
+    time = str(datetime.datetime.now().strftime("%H:%M"))
+    host = 'https://api.vasttrafik.se'
+    baseurl = '/bin/rest.exe/v2'
+    headers = {
+        'Authorization': 'Bearer ' + authKey
+    }
+    request = {
+        'id': id,
+        'date': date,
+        'time': time,
+        'format': 'json',
+    }
+    requestURL = host + baseurl + '/departureBoard'
+    r = requests.get(requestURL, params=request, headers=headers)
+    return json.dumps(r.json())
+
+def getAccsessTokenVasttrafik():
+    url = 'https://api.vasttrafik.se/token' # Set destination URL here
+    key = API['vasttrafik']['key']
+    secret = API['vasttrafik']['secret']
+    encoded = base64.b64encode((key + ":" + secret).encode()).decode()
+    post_fields = {
+        'grant_type': 'client_credentials',
+        'scope': '1'
+    }
+    headers = {
+        'Authorization': 'Basic ' + encoded,
+        'Content-Type': 'application/x-www-form-urlencoded',
+    }     # Set POST fields here
+    r = requests.post(url, data=post_fields, headers=headers).json()
+    r = json.dumps(r)
+    access_token = json.loads(r)['access_token']
+    return access_token
