82ca49320f800d40eed4656f97659eeef6751c14
Anton Levholm <anton.badminton@gmail.com>
2017-06-19 20:22:28 +0200

    structure Västtrafik data in a hacky way

--- flask-app/InfoHubAPI/vasttrafik.py
+++ flask-app/InfoHubAPI/vasttrafik.py
@@ -5,7 +5,7 @@ import base64
 from .api_keys import API
 
 def getVasttrafikJSON(id):
-    authKey = getAccsessTokenVasttrafik()
+    authKey = getAccessToken()
     date = '20' + str(datetime.datetime.now().strftime("%y-%m-%d"))
     time = str(datetime.datetime.now().strftime("%H:%M"))
     host = 'https://api.vasttrafik.se'
@@ -21,9 +21,10 @@ def getVasttrafikJSON(id):
     }
     requestURL = host + baseurl + '/departureBoard'
     r = requests.get(requestURL, params=request, headers=headers)
-    return json.dumps(r.json())
+    r = json.loads(json.dumps(r.json()))
+    return modifyJSON(r)
 
-def getAccsessTokenVasttrafik():
+def getAccessToken():
     url = 'https://api.vasttrafik.se/token' # Set destination URL here
     key = API['vasttrafik']['key']
     secret = API['vasttrafik']['secret']
@@ -40,3 +41,24 @@ def getAccsessTokenVasttrafik():
     r = json.dumps(r)
     access_token = json.loads(r)['access_token']
     return access_token
+
+def modifyJSON(data):
+    tmp = {}
+    result = {'Departure': []}
+    tmp['Departure'] = data['DepartureBoard']['Departure']
+    data = tmp
+    departures = {v['direction']:v for v in data['Departure']}
+    tmp_list = departures.keys()
+    for stop_id in tmp_list:
+        tmp = list(filter(lambda d: d['direction'] in stop_id, data['Departure']))
+        if len(tmp) > 1:
+            departures[stop_id]['rtTime'] = tmp[0]['rtTime']
+            departures[stop_id]['rtDate'] = tmp[0]['rtDate']
+            departures[stop_id]['nextRtTime'] = tmp[1]['rtTime']
+            departures[stop_id]['nextRtDate'] = tmp[1]['rtDate']
+            #departures[stop_id]['nextAccessibility'] = tmp[1]['accessibility']
+        del departures[stop_id]['JourneyDetailRef']
+        del departures[stop_id]['journeyid']
+        del departures[stop_id]['stopid']
+        result['Departure'].append(departures[stop_id])
+    return json.dumps(result)
