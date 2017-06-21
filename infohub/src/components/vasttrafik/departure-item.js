3348818c4a143990ec4eebcf84ce7bf5dbf9e938
Anton Levholm <anton.badminton@gmail.com>
2017-06-21 16:27:15 +0200

    remove unused code

--- infohub/src/components/vasttrafik/departure-item.js
+++ infohub/src/components/vasttrafik/departure-item.js
@@ -4,36 +4,18 @@ let moment = require('moment');
 
 class DepartureItem extends Component {
 
-  constructor(props) {
-      super(props);
-      const now = this.props.data.rtDate + ' ' + this.props.data.rtTime;
-      const next = this.props.data.nextRtDate + ' ' + this.props.data.nextRtTime;
-      this.state = {
-        departs: moment(now, "YYYY-MM-DD HH:mm").fromNow(),
-        nextDeparture: moment(next, "YYYY-MM-DD HH:mm").fromNow()
-      }
-  }
-
-  componentDidMount() {
-    const now = this.props.data.rtDate + ' ' + this.props.data.rtTime;
-    const next = this.props.data.nextRtDate + ' ' + this.props.data.nextRtTime;
-     window.setInterval(function() {
-      this.setState({
-        departs: moment(now, "YYYY-MM-DD HH:mm").fromNow(),
-        nextDeparture: moment(next, "YYYY-MM-DD HH:mm").fromNow()
-      });
-    }.bind(this), 1000);
-  }
-
   render() {
+
     const spanStyle = {
       color: this.props.data.bgColor,
       backgroundColor: this.props.data.fgColor,
       padding: '0.1em 0.3em',
       borderRadius: '0.2em'
     };
+
     const now = this.props.data.rtDate + ' ' + this.props.data.rtTime;
     const next = this.props.data.nextRtDate + ' ' + this.props.data.nextRtTime;
+
     return (
       <li className="widget-list-item">
         <div className="line">
