81701029d5321a1d372e64ba0e6904f57a260877
Anton Levholm <anton.badminton@gmail.com>
2017-06-21 21:24:43 +0200

    add Dark Sky API

--- flask-app/InfoHubAPI/app.py
+++ flask-app/InfoHubAPI/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, Response
 from .vasttrafik import getVasttrafikJSON
+from .darksky import getDarkSkyJSON
 app = Flask(__name__)
 
 @app.route('/')
@@ -9,5 +10,12 @@ def hello_world():
 @app.route('/vasttrafik/<id>')
 def vasttrafik(id=None):
     resp = Response(getVasttrafikJSON(id))
+    resp.headers['Access-Control-Allow-Origin'] = 'http://localhost:3000'
+    return resp
+
+@app.route('/darksky/<location>')
+# location format: [latitude],[longitude]
+def darksky(location=None):
+    resp = Response(getDarkSkyJSON(location))
     resp.headers['Access-Control-Allow-Origin'] = 'http://localhost:3000'
     return resp
---
+++ flask-app/InfoHubAPI/darksky.py
@@ -0,0 +1,19 @@
+import json
+import requests
+from .api_keys import API
+
+def getDarkSkyJSON(location):
+    authKey = API['darkSky']['secret']
+    host = 'https://api.darksky.net'
+    baseurl = '/forecast/'
+    request = {
+        'lang': 'sv',
+        'units': 'si',
+    }
+    requestURL = host + baseurl + authKey + '/' + location
+    r = requests.get(requestURL, params=request)
+    r = json.loads(json.dumps(r.json()))
+    return modifyJSON(r)
+
+def modifyJSON(data):
+    return json.dumps(data)
